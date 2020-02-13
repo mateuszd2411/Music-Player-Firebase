818899abae779a102a4fd8bac0c0f424d98b1034
Mateusz <mateuszd2411@gmail.com>
2020-02-13 10:02:53 +0100

    MP Firebase 38:06

--- app/src/main/java/com/example/musicplayerfirebase/MainActivity.java
+++ app/src/main/java/com/example/musicplayerfirebase/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -12,7 +13,17 @@ import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -24,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean checkPermission = false;
     Uri uri;
-    String songName;
+    String songName,songUrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,12 +86,73 @@ public class MainActivity extends AppCompatActivity {
                 songName = mcursor.getString(indexwdname);
                 mcursor.close();
 
+                uploadSongToFirebaseStorage();
 
 
             }
         }
 
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadSongToFirebaseStorage() {
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference()
+                .child("Songs").child(uri.getLastPathSegment());
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.show();
+
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while (!uriTask.isComplete());
+                Uri urlSong = uriTask.getResult();
+                songUrl = urlSong.toString();
+
+                uploadDetailsToDatabase();
+                progressDialog.dismiss();
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                double progres = (100.0*taskSnapshot.getBytesTransferred())/taskSnapshot.getTotalByteCount();
+                int currentProgress = (int)progres;
+                progressDialog.setMessage("Uploaded: "+currentProgress+" %");
+            }
+        });
+
+    }
+
+    private void uploadDetailsToDatabase() {
+
+        Song songObj = new Song(songName,songUrl);
+
+        FirebaseDatabase.getInstance().getReference("Songs")
+                .push().setValue(songObj).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Song Uploaded", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private boolean validatePermision(){
