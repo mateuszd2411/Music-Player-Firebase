81f01363e53e00d6e270af75d8fb42fc068ab4a3
Mateusz <mateuszd2411@gmail.com>
2020-02-13 10:18:53 +0100

    MP Firebase 50:11

--- app/src/main/java/com/example/musicplayerfirebase/MainActivity.java
+++ app/src/main/java/com/example/musicplayerfirebase/MainActivity.java
@@ -13,13 +13,19 @@ import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
@@ -31,15 +37,51 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private boolean checkPermission = false;
     Uri uri;
     String songName,songUrl;
+    ListView listView;
+
+    ArrayList<String> arrayListSongsName = new ArrayList<>();
+    ArrayList<String> arrayListSongsUrl = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.myListView);
+
+        retrieveSongs();
+    }
+
+    private void retrieveSongs() {
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Songs");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds :dataSnapshot.getChildren()){
+
+                    Song songObj = ds.getValue(Song.class);
+                    arrayListSongsName.add(songObj.getSongName());
+                    arrayListSongsUrl.add(songObj.getSongUrl());
+                }
+
+                arrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, arrayListSongsName);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
